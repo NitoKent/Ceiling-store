765b59b92b49f1121a260f6891442b6b6c3074df
Daniel Kirushin <retrostyae@mail.ru>
2024-08-02 09:00:04 +0300

    NavBar

--- src/scenes/NavBar.tsx
+++ src/scenes/NavBar.tsx
@@ -9,11 +9,27 @@ export function NavBar(props: Props) {
     return (
         <nav>
             <div
-                className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+                className={`${flexBetween} fixed top-0 z-30 w-full py-6 `}>
                 <div className={`${flexBetween} mx-auto w-5/6`}>
-                    <div className={`${flexBetween} w-full gap-16`}>
+                    <div className={`${flexBetween} w-full gap-1`}>
+                        {/* LEFT SIDE */}
                         <img alt="logo" src={logo} />
+                        <p className=" font-semibold text-white-20 ">LEO CEILING</p>
 
+                        {/* RIGHT SIDE */}
+                         <div className={`${flexBetween} w-full pl-10 `}>
+                            <div className={`${flexBetween} ml-10 gap-8 text-sm`}>
+                                <p>Home</p>
+                                <p>About</p>
+                                <p>Works</p>
+                                <p>Calculeted</p>
+                            </div>
+                            <div className={`${flexBetween} gap-3`}>
+                                <button>Send Work</button>
+
+                            </div>
+
+                         </div>
                     </div>
 
                 </div>
